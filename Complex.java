5e12b6058865ac6063c4422211073317b47e47cb
Ashutosh Ayush <jbnashu297@gmail.com>
2023-12-26 14:19:33 +0530

    Class and Object

--- Complex.java
+++ Complex.java
@@ -1,3 +1,25 @@
+import java.util.Scanner;
+class complex{
+    int a,b;
+    int sum(){
+        int c=a+b;
+        return c;
+    }
+    int diff(){
+        int c=a-b;
+        return c;
+    }
+    int pro(){
+        int c=a*b;
+        return c;
+    }
+}
 public class Complex {
-    
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the Value of  a & b");
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        
+    }
 }
