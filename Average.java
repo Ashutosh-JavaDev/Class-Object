6c84480788213144a9e2bc38cebe7407c2854118
Ashutosh Ayush <jbnashu297@gmail.com>
2023-12-26 13:37:14 +0530

    Class and Object

--- Average.java
+++ Average.java
@@ -1,17 +1,28 @@
 import java.util.*;
-class avg{
+
+class avg {
     int arr[];
-    float sum=0,a=0;
-    void takingInput(int size){
-        Scanner sc=new Scanner(System.in);
+    float sum = 0, a = 0;
+
+    void takingInput(int size) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter the size of Array");
-        size=sc.nextInt();
+        size = sc.nextInt();
     }
-    void cal(){
+
+    void cal() {
         takingInput(size);
-        for(int i=0;i<size;i++){
-            sum+=arr[i];
+        for (int i = 0; i < size; i++) {
+            sum += arr[i];
         }
-        
+        a = sum / size;
+        System.out.println("Average:    " + a);
+    }
+}
+
+public class Average {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
     }
 }
