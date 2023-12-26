c28c8cdc97cd748d226474ebbf649558bbcd1d6d
Ashutosh Ayush <jbnashu297@gmail.com>
2023-12-26 09:29:24 +0530

    Class and Object

---
+++ aa1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class aa1{
+    int a,b;
+    static void get(int x,int y){
+        Scanner sc=new Scanner(System.in);
+        System.out.println(("Enter the Value of A & B"));
+        x=sc.nextInt();
+        y=sc.nextInt();
+    }
+    static void put(){
+        
+    }
+}
