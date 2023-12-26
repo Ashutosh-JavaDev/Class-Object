7361eb4d6d2f2c405cb13d95549bffae04608ead
Ashutosh Ayush <jbnashu297@gmail.com>
2023-12-26 09:36:14 +0530

    Class and Object

--- swap.java
+++ swap.java
@@ -4,10 +4,7 @@ class aa1 {
     int a, b;
 
     void get(int x, int y) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println(("Enter the Value of A & B"));
-        x = sc.nextInt();
-        y = sc.nextInt();
+
         a = x;
         b = y;
     }
@@ -21,5 +18,18 @@ class aa1 {
     void disp() {
         System.out.println("Value of A after swap: " + a);
         System.out.println("Value  of B after Swap: " + b);
+    }
+}
+
+public class swap {
+    public static void main(String[] args) {
+        aa1 ob = new aa1();
+        Scanner sc = new Scanner(System.in);
+        System.out.println(("Enter the Value of A & B"));
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        ob.get(x, y);
+        ob.put();
+        ob.disp();
     }
 }
