f292e30e1ca3d2e838b01c68595f4181b2702434
Ashutosh Ayush <jbnashu297@gmail.com>
2023-12-26 09:51:58 +0530

    Class And Object ;

--- value.java
+++ value.java
@@ -19,10 +19,27 @@ class aa2 {
     }
 
     void put() {
+        System.out.println("Name:   " + Name);
+        System.out.println("Roll:   " + Roll);
+        System.out.println("Fee:    " + fee);
+        System.out.println("-------------------------------------------------------------------");
 
     }
 }
 
 public class value {
-
+    public static void main(String[] args) {
+        aa2[] ob = new aa2[5];
+        for (int i = 0; i < 5; i++) {
+            ob[i] = new aa2();
+        }
+        System.out.println("Enter Student Details");
+        for (int i = 0; i < 5; i++) {
+            ob[i].get();
+        }
+        System.out.println("Student Details");
+        for (int i = 0; i < 5; i++) {
+            ob[i].put();
+        }
+    }
 }
