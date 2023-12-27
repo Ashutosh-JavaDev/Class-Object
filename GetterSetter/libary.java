ab793f5de6432e31e351a56a72fc8ffca08502b4
Ashutosh Ayush <jbnashu297@gmail.com>
2023-12-27 17:06:42 +0530

    Java Program

--- GetterSetter/libary.java
+++ GetterSetter/libary.java
@@ -1,5 +1,38 @@
 package GetterSetter;
 
- class libary {
-    
+import java.util.*;
+
+class libary {
+    String Books, arr[];
+    int Press;
+
+    void getName(String Books) {
+        this.Books = Books;
+    }
+
+    String setName() {
+        return Books;
+    }
+
+    void Lib(int Press) {
+        Scanner sc = new Scanner(System.in);
+        this.Press = Press;
+        switch (Press) {
+            case 1:
+                System.out.println("How Many Books Want to Add In Libary");
+                int Number = sc.nextInt();
+                sc.nextLine();
+                arr = new String[Number];
+
+                if (Number < 1) {
+                    System.out.println("Libary is Empty");
+                } else {
+                    System.out.println("Enter Name of the Books wants to add in the Libary");
+                    for (int i = 0; i < Number; i++) {
+                        arr[i] = sc.nextLine();
+                    }
+
+                }
+        }
+    }
 }
