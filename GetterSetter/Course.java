8872653c7c242391b57edadabb09b86da0b88562
Ashutosh Ayush <jbnashu297@gmail.com>
2023-12-27 15:53:25 +0530

    Java Program

--- GetterSetter/Course.java
+++ GetterSetter/Course.java
@@ -1,5 +1,22 @@
 package GetterSetter;
 
+import java.util.*;
+
 public class Course {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        course ob = new course();
+        System.out.println("Enter Student Name");
+        ob.getName(sc.nextLine());
+        System.out.println("Enter Grade");
+        ob.getGrade(sc.nextLine().charAt(0));
+        System.out.println("Enter Present Course Name");
+        ob.getCourse(sc.nextLine());
+        System.out.println("Name:               " + ob.setName());
+        System.out.println("Grade:              " + ob.setGrade());
+        System.out.println("Course:             " + ob.setCourse());
+        System.out.println("Press1.to add Courses\nPress2.to remove Courses");
+
+        ob.Courses();
+    }
 }
