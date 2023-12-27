8c0af25d856128ef7935b4f037780223f3177d57
Ashutosh Ayush <jbnashu297@gmail.com>
2023-12-27 10:37:34 +0530

    Java Program

---
+++ GetterSetter/aa3.java
@@ -0,0 +1,13 @@
+package GetterSetter;
+import java.util.*;
+ class aa3 {
+    String Name;
+    int year;
+    Double Amount;
+    int press,Add,Del;
+    void getdata(String Name,int year,Double Amount){
+        this.Name=Name;
+        this.year=year;
+        this.Amount=Amount;
+    }
+}
