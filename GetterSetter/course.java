0dc82cc02ac05f05fa3df2c3c1f6b69d04898ac5
Ashutosh Ayush <jbnashu297@gmail.com>
2023-12-27 15:33:48 +0530

    Java Program

--- GetterSetter/course.java
+++ GetterSetter/course.java
@@ -1,5 +1,32 @@
 package GetterSetter;
 
-public class course {
-    
+class course {
+    String Name, Course, arr[];
+    char Grade;
+    int press;
+
+    void getName(String Name) {
+        this.Name = Name;
+    }
+
+    String setName() {
+        return Name;
+    }
+
+    void getCourse(String Course) {
+        this.Course = Course;
+    }
+
+    String setCourse() {
+        return Course;
+    }
+
+    void getGrade(char Grade) {
+        this.Grade = Grade;
+    }
+
+    char setGrade() {
+        return Grade;
+    }
+
 }
