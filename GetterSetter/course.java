cf1690fdd86e2be10af9010a64873692a3123bab
Ashutosh Ayush <jbnashu297@gmail.com>
2023-12-27 15:39:14 +0530

    Java Program

--- GetterSetter/course.java
+++ GetterSetter/course.java
@@ -1,9 +1,9 @@
 package GetterSetter;
-
+import java.util.*;
 class course {
     String Name, Course, arr[];
     char Grade;
-    int press;
+    int Press;
 
     void getName(String Name) {
         this.Name = Name;
@@ -29,4 +29,18 @@ class course {
         return Grade;
     }
 
+    void Courses(){
+        Scanner sc=new Scanner(System.in);
+        this.Press=Press;
+        switch(Press){
+            case 1:
+            System.out.println("How Many New Subject Wants to Add");
+            int number=sc.nextInt();
+            arr=new String[number];
+            System.out.println("Enter The name of the courses");
+            for(int i=0;i<number;i++){
+                arr[i]=sc.nextLine();
+            }
+        }        
+    }
 }
