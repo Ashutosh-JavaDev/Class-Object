26aeacbd32cce241f084fdb75b37120a84e8c20a
Ashutosh Ayush <jbnashu297@gmail.com>
2023-12-27 17:18:51 +0530

    Java Program

--- GetterSetter/libary.java
+++ GetterSetter/libary.java
@@ -3,15 +3,23 @@ package GetterSetter;
 import java.util.*;
 
 class libary {
-    String Books, arr[];
+    String arr1[], arr[];
     int Press;
 
-    void getName(String Books) {
-        this.Books = Books;
-    }
-
-    String setName() {
-        return Books;
+    void getName(String arr1[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("How Many Books want to Insert In Libary");
+        int Number = sc.nextInt();
+        sc.nextLine();
+        arr1 = new String[Number];
+        System.out.println("Enter Books Name");
+        for (int i = 0; i < Number; i++) {
+            arr1[i] = sc.nextLine();
+        }
+        System.out.println("Books Present in Libary");
+        for (int i = 0; i < Number; i++) {
+            System.out.println(arr1[i]);
+        }
     }
 
     void Lib(int Press) {
@@ -19,17 +27,24 @@ class libary {
         this.Press = Press;
         switch (Press) {
             case 1:
-                System.out.println("How Many Books Want to Add In Libary");
+                System.out.println("How Many New Books Want to Add In Libary");
                 int Number = sc.nextInt();
                 sc.nextLine();
                 arr = new String[Number];
 
                 if (Number < 1) {
-                    System.out.println("Libary is Empty");
+                    for (int i = 0; i < Number; i++) {
+                        arr[i] = arr1[i];
+                    }
+                    System.out.println("No New Books Added");
                 } else {
                     System.out.println("Enter Name of the Books wants to add in the Libary");
                     for (int i = 0; i < Number; i++) {
                         arr[i] = sc.nextLine();
+                    }
+                    System.out.println("Libary After Updated");
+                    for (int i = 0; i < Number; i++) {
+                        System.out.println(arr[i]);
                     }
 
                 }
