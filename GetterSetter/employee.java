af4ba4b28daa75df609c07d3eee605c8b48fd04d
Ashutosh Ayush <jbnashu297@gmail.com>
2023-12-27 14:40:01 +0530

    Java Program

--- GetterSetter/employee.java
+++ GetterSetter/employee.java
@@ -1,5 +1,15 @@
 package GetterSetter;
+
 class employee {
     String Name;
-    int Salary,year;
+    int Salary, year;
+
+    void getName(String Name) {
+        this.Name = Name;
+    }
+
+    String setName() {
+        return Name;
+    }
+
 }
