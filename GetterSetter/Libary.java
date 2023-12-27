4249add847f8f7156d6f75cf7f8ca9c02e587005
Ashutosh Ayush <jbnashu297@gmail.com>
2023-12-27 17:29:23 +0530

    Java Program

--- GetterSetter/Libary.java
+++ GetterSetter/Libary.java
@@ -1,5 +1,12 @@
 package GetterSetter;
-
+import java.util.*;
 public class Libary {
-    
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        libary ob=new libary();
+        ob.getName(ob.arr1);
+        System.out.println("Press 1 to add new books\nPress 2 to remove books");
+        int userChoice = sc.nextInt();
+        ob.Lib(userChoice);
+    }
 }
