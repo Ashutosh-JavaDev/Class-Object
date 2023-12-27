3f01012c063fae51f4abb9187db075f8b05119b5
Ashutosh Ayush <jbnashu297@gmail.com>
2023-12-27 09:35:37 +0530

    Java Program

---
+++ aa2.java
@@ -0,0 +1,18 @@
+class aa2 {
+    private int age;
+    private String Name;
+    private char Gender;
+    private double fare;
+
+    void getAge(int age) {
+        this.age = age;
+    }
+
+    int setAge() {
+        return age;
+    }
+
+    void getName(String Name) {
+
+    }
+}
