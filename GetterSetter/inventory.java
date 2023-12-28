6d844d791071c169aeb3276c53975b256fe27882
Ashutosh Ayush <jbnashu297@gmail.com>
2023-12-28 12:28:28 +0530

    Java Program

--- GetterSetter/inventory.java
+++ GetterSetter/inventory.java
@@ -1,5 +1,27 @@
 package GetterSetter;
-
-public class inventory {
-    
+import java.util.Scanner;
+class inventory {
+    String HighInventory[],LowInventory[];
+    int Press;
+    void geyHighInventory(String HighInventory[]){
+        this.HighInventory=HighInventory;
+    }
+    String[] setHighInventory(){
+        return HighInventory;
+    }
+    void getLowInventory(String LowInventory[]){
+        this.LowInventory=LowInventory;
+    }
+    String[] setLowInventory(){
+        return LowInventory;
+    }
+    void disp(){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Press1.to Add Product\nPress2.to Remove Product");
+        int press=sc.nextInt();
+        switch(press){
+            case 1:
+            
+        }
+    }
 }
