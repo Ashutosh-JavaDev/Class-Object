593a5331211d3ddfb31262517adacae256657665
Ashutosh Ayush <jbnashu297@gmail.com>
2023-12-28 09:28:14 +0530

    Java Program

--- DateandTime/Loctime.java
+++ DateandTime/Loctime.java
@@ -1,9 +1,21 @@
 package DateandTime;
+
+import java.time.Instant;
+import java.time.Duration;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 
 public class Loctime {
     public static void main(String[] args) {
         LocalTime currentTime = LocalTime.now();
         System.out.println("Current Time: " + currentTime);
+        LocalDateTime currentDateTime = LocalDateTime.now();
+        System.out.println("currentDateTime:    " + currentDateTime);
+        Instant instantTime = Instant.now();
+        System.out.println("instantTime:    " + instantTime);
+        Instant start = Instant.now();
+        Instant end = Instant.now();
+        Duration duration = Duration.between(start, end);
+        System.out.println("Duration:   "+duration);
     }
 }
