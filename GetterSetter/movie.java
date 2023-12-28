b4825174e41a0e45e8d1c28509c6ff4760246fc3
Ashutosh Ayush <jbnashu297@gmail.com>
2023-12-28 17:06:28 +0530

    Java Program

--- GetterSetter/movie.java
+++ GetterSetter/movie.java
@@ -1,5 +1,7 @@
 package GetterSetter;
 
+import java.util.Scanner;
+
 class movie {
     String Title, Actor, Director;
     int Review;
@@ -9,5 +11,28 @@ class movie {
         this.Actor = Actor;
         this.Director = Director;
         this.Review = Review;
+    }
+
+    void disp() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Press Add to add review\nPress Remove to Remove review\nPress No to make review Default");
+        String rev = sc.nextLine();
+        switch (rev.toLowerCase()) {
+            case "add":
+                System.out.println("How Much Review want to add for Movie");
+                int addRev = sc.nextInt();
+                System.out.println("Updated review:     " + (addRev + Review));
+                break;
+
+            case "remove":
+                System.out.println("How Much Review want to Remove for Movie");
+                int remRev = sc.nextInt();
+                System.out.println("Updated review:     " + (remRev - Review));
+                break;
+            default:
+                System.out.println("Invalid Press");
+                break;
+        }
+
     }
 }
