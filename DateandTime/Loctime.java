c0051237f64969d9a50aa26ca91dbf0a15cdea30
Ashutosh Ayush <jbnashu297@gmail.com>
2023-12-28 09:25:13 +0530

    Java Program

---
+++ DateandTime/Loctime.java
@@ -0,0 +1,9 @@
+package DateandTime;
+import java.time.LocalTime;
+
+public class Loctime {
+    public static void main(String[] args) {
+        LocalTime currentTime = LocalTime.now();
+        System.out.println("Current Time: " + currentTime);
+    }
+}
