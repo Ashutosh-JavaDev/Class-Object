af02c092844c3384c115cbf0753b6cf2295d6d63
Ashutosh Ayush <jbnashu297@gmail.com>
2023-12-28 22:11:59 +0530

    Java Program

--- GetterSetter/resturant.java
+++ GetterSetter/resturant.java
@@ -1,5 +1,44 @@
 package GetterSetter;
 
-public class resturant {
-    
+import java.util.Scanner;
+
+class resturant {
+    public String Menuname[];
+    float Price, resturantRating;
+    int totalItem;
+
+    void getData(String[] Menuname, float Price, float resturantRating, int totalItem) {
+        this.Menuname = Menuname;
+        this.Price = Price;
+        this.resturantRating = resturantRating;
+        this.totalItem = totalItem;
+    }
+
+    String[] setData() {
+        return Menuname;
+    }
+
+    void disp() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Press 1.to add Item in the List\nPress 2. to remove item from the list");
+        int Press = sc.nextInt();
+        switch (Press) {
+            case 1:
+                System.out.println("How many item want to add list");
+                int newItem = sc.nextInt();
+                String newItemName[] = new String[newItem];
+                System.out.println("Enter Item Name");
+                for (int i = 0; i < newItem; i++) {
+                    newItemName[i] = sc.nextLine();
+                }
+                System.out.println("List after Updated");
+                for (int i = 0; i < Menuname.length; i++) {
+                    System.out.println(Menuname[i]);
+                }
+                for (int i = 0; i < newItemName.length; i++) {
+                    System.out.println(newItemName[i]);
+                }
+                break;
+        }
+    }
 }
