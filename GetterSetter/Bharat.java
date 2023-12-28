0f0d6ee1d88d1dad16ba27482191d6bce03290f4
Ashutosh Ayush <jbnashu297@gmail.com>
2023-12-28 23:21:22 +0530

    Java Program

--- GetterSetter/Bharat.java
+++ GetterSetter/Bharat.java
@@ -1,5 +1,12 @@
 package GetterSetter;
-
 public class Bharat {
-    
+    public static void main(String[] args) {
+        bharat ob=new bharat();
+        ob.getFlight();;
+        ob.getHotel();
+        ob.Hotelbook();
+        ob.FlightBook();
+        ob.HotelCancel();
+        ob.FlightCancel();
+    }
 }
