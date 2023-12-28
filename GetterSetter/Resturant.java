5de8de54ebdfd8cc210d9a9e444d8644fd69fd71
Ashutosh Ayush <jbnashu297@gmail.com>
2023-12-28 22:43:11 +0530

    Java Program

--- GetterSetter/Resturant.java
+++ GetterSetter/Resturant.java
@@ -8,16 +8,21 @@ public class Resturant {
         resturant ob = new resturant();
         System.out.println("How many Item You want to add in your resturant at present");
         ob.gettotalItem(sc.nextInt());
+        sc.nextLine();
         String Menu[] = new String[ob.totalItem];
-        Float RestPrice[]=new Float[ob.totalItem];
+        Float RestPrice[] = new Float[ob.totalItem];
         ob.getMenu(Menu);
         System.out.println("Enter Item Name and Price In Resturant List");
         for (int i = 0; i < ob.totalItem; i++) {
             System.out.println("Item Name:");
-            Menu[i]=sc.nextLine();
+            Menu[i] = sc.nextLine();
             System.out.println("Item Price:");
-            RestPrice[i]=sc.nextFloat();
+            RestPrice[i] = sc.nextFloat();
         }
-
+        System.out.println("What is the Rating Of your Resturant");
+        ob.getresurantRating(sc.nextFloat());
+        ob.disp();
+        System.out.println(ob.setData() + ":   " + ob.setPrice());
+        System.out.println("Average Rating of Your Resturant:       "+ob.calAvgerage());
     }
 }
