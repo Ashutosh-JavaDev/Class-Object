7010fc9079092baadeac76a9112839bb6ace1415
Ashutosh Ayush <jbnashu297@gmail.com>
2023-12-28 15:10:16 +0530

    java Program

--- GetterSetter/MusicLibary.java
+++ GetterSetter/MusicLibary.java
@@ -1,5 +1,10 @@
 package GetterSetter;
-
+import java.util.*;
 public class MusicLibary {
-    
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("How Many Song You wang to Load in you Music Libary");
+        int Number=sc.nextInt();
+        
+    }
 }
