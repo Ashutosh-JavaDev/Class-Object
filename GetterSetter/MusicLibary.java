8c8c7eb0fbf00b19e80d6794d8c250cb76477f66
Ashutosh Ayush <jbnashu297@gmail.com>
2023-12-28 15:19:29 +0530

    java Program

--- GetterSetter/MusicLibary.java
+++ GetterSetter/MusicLibary.java
@@ -5,9 +5,13 @@ import java.util.*;
 public class MusicLibary {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("How Many Song You wang to Load in you Music Libary");
+        System.out.println("How Many Song You want to Load in you Music Libary");
         int Number = sc.nextInt();
+        String str[]=new String[Number];
         musiclibary ob = new musiclibary();
-
+        System.out.println("Enter the Name of Songs you want to Insert in your List");
+        for(int i=0;i<Number;i++){
+            str[i]=sc.nextLine();   
+        }
     }
 }
