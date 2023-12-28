86937bdeea2367825150540e4ca3acbb111386ac
Ashutosh Ayush <jbnashu297@gmail.com>
2023-12-28 23:09:41 +0530

    Java Program

--- GetterSetter/bharat.java
+++ GetterSetter/bharat.java
@@ -1,5 +1,19 @@
 package GetterSetter;
 
-public class bharat {
-    
+import java.util.*;
+
+class bharat {
+    String Flightname[], Hotelnamee[];
+
+    void getFlight() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Insert How Many Hotels You want to Insert in Your app");
+        int HotelNumber = sc.nextInt();
+        sc.nextLine();
+        String HotelName[] = new String[HotelNumber];
+        System.out.println("Insert name of the Hotel");
+        for (int i = 0; i < HotelNumber; i++) {
+            HotelName[i] = sc.nextLine();
+        }
+    }
 }
