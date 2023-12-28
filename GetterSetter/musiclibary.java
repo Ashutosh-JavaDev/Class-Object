06d50095efc34d7d4b1ffb1c6ba0015bdb0e3a71
Ashutosh Ayush <jbnashu297@gmail.com>
2023-12-28 14:43:07 +0530

    java Program

--- GetterSetter/musiclibary.java
+++ GetterSetter/musiclibary.java
@@ -1,5 +1,11 @@
 package GetterSetter;
-
-public class musiclibary {
-    
+import java.util.*;
+class musiclibary {
+    String SongCollection[];
+    void getSong(String SongCollection[]){
+        this.SongCollection=SongCollection;
+    }    
+    String[] setSong(){
+        return SongCollection;
+    }
 }
