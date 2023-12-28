43fd4c411d770d186e14c09fb91ffe1de8f5db8c
Ashutosh Ayush <jbnashu297@gmail.com>
2023-12-28 17:10:20 +0530

    Java Program

--- GetterSetter/Movie.java
+++ GetterSetter/Movie.java
@@ -1,5 +1,17 @@
 package GetterSetter;
-
+import java.util.*;
 public class Movie {
-    
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        movie ob=new movie();
+        System.out.println("Enter the title  of Movie");
+        ob.getdata(sc.nextLine(), null, null, 0);
+        System.out.println("Enter movie's Actor Name");
+        ob.getdata(null, sc.nextLine(), null, 0);
+        System.out.println("Enter Movie Director Name");
+        ob.getdata(null, null, sc.nextLine(), 0);
+        System.out.println("Enter Current Movie  Review");
+        ob.getdata(null, null, null, sc.nextInt());
+        ob.disp();
+    }
 }
