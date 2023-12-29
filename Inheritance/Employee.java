9a7345fa319e524ca53ecd135f13825ad11d4c14
Ashutosh Ayush <jbnashu297@gmail.com>
2023-12-29 13:07:23 +0530

    Java Program

--- Inheritance/Employee.java
+++ Inheritance/Employee.java
@@ -56,5 +56,13 @@ class HrManager extends employee {
 }
 
 public class Employee {
-
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        HrManager ob=new HrManager();
+        System.out.println("Enter Employee Work Degination");
+        ob.getWork(sc.nextLine());
+        System.out.println("Enter Employee Salary");
+        ob.getSalary(sc.nextFloat());
+        ob.addEmployee();
+    }
 }
