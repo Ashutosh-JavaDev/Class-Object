e36ad305cc8b315b6d0b7ecd5b825fca8caa874e
Ashutosh Ayush <jbnashu297@gmail.com>
2023-12-29 11:44:39 +0530

    Java Program

---
+++ Inheritance/Vehicle.java
@@ -0,0 +1,19 @@
+package Inheritance;
+class  vehicle{
+    String Drive(){
+        String status="Reparing a Car";
+        return status;
+    }
+}
+class Car extends vehicle{
+    void driven(){
+        System.out.println(("Status:    "+Drive()));
+
+    }
+}
+public class Vehicle {
+   public static void main(String[] args) {
+     Car ob=new Car();
+    ob.driven();
+   }
+}
