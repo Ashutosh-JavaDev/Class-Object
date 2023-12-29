95f35fc606f846bcd49327812134545ba7187fc7
Ashutosh Ayush <jbnashu297@gmail.com>
2023-12-29 12:39:13 +0530

    Java Program

---
+++ Inheritance/Employee.java
@@ -0,0 +1,21 @@
+package Inheritance;
+import java.util.*;
+class employee{
+    String work;
+    float salary;
+    void getWork(String work){
+        this.work=work;
+    }
+    String setWork(){
+        return work;
+    }
+    void getSalary(float salary){
+        this.salary=salary;
+    }
+    float setSalary(){
+        return salary;
+    }
+}
+public class Employee {
+    
+}
