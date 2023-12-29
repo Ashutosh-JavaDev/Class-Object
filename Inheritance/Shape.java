d7b445a818a38e71a7ffd08b975671db4b51684e
Ashutosh Ayush <jbnashu297@gmail.com>
2023-12-29 11:53:09 +0530

    Java Program

---
+++ Inheritance/Shape.java
@@ -0,0 +1,17 @@
+package Inheritance;
+class shape{
+    float a,b;
+    float getArea(){
+        float area=a*b;
+        return area;
+    }
+}
+class rectangle extends shape{
+    void getRect(float length,float breadth){
+        
+    }
+
+}
+public class Shape {
+    
+}
