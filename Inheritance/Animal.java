0736831be50dc75e420ee9841605215a26cd97ae
Ashutosh Ayush <jbnashu297@gmail.com>
2023-12-29 10:46:11 +0530

    Java Program

--- Inheritance/Animal.java
+++ Inheritance/Animal.java
@@ -1,5 +1,22 @@
 package Inheritance;
 import java.util.Scanner;
+class animal{
+    String sound;
+    void makeSound(){
+        sound="Meow";
+    }
+    String putSound(){
+        return sound;
+    }
+}
+class cat extends animal{
+    void Voice(){
+        System.out.println("Cat:    "+putSound());
+    }
+}
 public class Animal {
-    
+    public static void main(String[] args) {
+        cat ob=new cat();
+        ob.Voice();
+    }
 }
