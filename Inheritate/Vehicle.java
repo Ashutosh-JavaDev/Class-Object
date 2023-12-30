81a8b2ebb44e66ea84707d23d917b9cb8ca8a483
Ashutosh Ayush <jbnashu297@gmail.com>
2023-12-30 14:37:16 +0530

    Java Program

--- Inheritate/Vehicle.java
+++ Inheritate/Vehicle.java
@@ -8,7 +8,24 @@ class vehicle{
         this.FuelType=FuelType;
         this.Year=Year;
     }
-    
+    String setModelName(){
+        return Modelname;
+    }
+    String setFuelType(){
+        return FuelType;
+    }
+    int setYear(){
+        return Year;
+    }
+    float calFuel(){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Insert How Much Distance Travel\nNote:insert only number");
+        float distance=sc.nextFloat();
+        System.out.println("Fuel Consumed while travelling?");
+        float fuel=sc.nextFloat();
+        float calculate=distance/fuel;
+        return calculate;
+    }
 }
 public class Vehicle {
     
