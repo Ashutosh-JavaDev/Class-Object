77e63b7fc479964c31680eebc5dc6baecbcb16a0
Ashutosh Ayush <jbnashu297@gmail.com>
2023-12-30 23:09:55 +0530

    Java Program

--- Inheritate/Employee.java
+++ Inheritate/Employee.java
@@ -1,11 +1,46 @@
 package Inheritate;
+
 import java.util.Scanner;
-class employee{
-    String name[],Address[],Jobtitle[];
-    void getDetail(){
-        Scanner sc=new Scanner(System.in);
+
+class employee {
+    String name[], Address[], Jobtitle[];
+    int Salary;
+
+    void getDetail() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Insert Number of Employee detail Want to add in Data");
+        int Number = sc.nextInt();
+        sc.nextLine();
+        name = new String[Number];
+        Address = new String[Number];
+        Jobtitle = new String[Number];
+        System.out.println("Enter Employee Datail");
+        System.out.println("-----------------------------------------------------");
+        for (int i = 0; i < Number; i++) {
+            System.out.println("Enter Employee Name");
+            name[i] = sc.nextLine();
+            System.out.println("Address of Employee");
+            Address[i] = sc.nextLine();
+            System.out.println("Job Degination");
+            Jobtitle[i] = sc.nextLine();
+        }
+    }
+
+    void GetSalary(int Salary) {
+        this.Salary = Salary;
+    }
+
+    int setSalary() {
+        return Salary;
+    }
+
+    float CalBonus() {
+        float bonus = Salary*8.33f/100;
+        return bonus;
+
     }
 }
+
 public class Employee {
-    
+
 }
