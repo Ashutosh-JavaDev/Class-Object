6d9836294cb739e911b508f726ef390adc55d041
Ashutosh Ayush <jbnashu297@gmail.com>
2023-12-30 22:49:19 +0530

    Java Program

--- Inheritate/Employee.java
+++ Inheritate/Employee.java
@@ -1,6 +1,10 @@
-import java.utill.Scanner;
+package Inheritate;
+import java.util.Scanner;
 class employee{
-    
+    String name[],Address[],Jobtitle[];
+    void getDetail(){
+        Scanner sc=new Scanner(System.in);
+    }
 }
 public class Employee {
     
