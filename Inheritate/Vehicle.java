82c9fc0268c8e86d44da3d2492152ff49a1c8274
Ashutosh Ayush <jbnashu297@gmail.com>
2023-12-30 14:28:42 +0530

    Java Program

---
+++ Inheritate/Vehicle.java
@@ -0,0 +1,15 @@
+package Inheritate;
+import java.util.*;
+class vehicle{
+    String Modelname,FuelType;
+    int Year;
+    void getInput(String Modelname, String FuelType,int Year){
+        this.Modelname=Modelname;
+        this.FuelType=FuelType;
+        this.Year=Year;
+    }
+    
+}
+public class Vehicle {
+    
+}
