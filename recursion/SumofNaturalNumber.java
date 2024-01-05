c882be2587d2f44103cf1f0de801726a7fbed72e
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-05 16:33:53 +0530

    JAva Program

--- recursion/SumofNaturalNumber.java
+++ recursion/SumofNaturalNumber.java
@@ -2,6 +2,16 @@ package recursion;
 
 import java.util.Scanner;
 
+class findSum {
+    int sumofOutput(int i) {
+        if (i > 10) {
+            return 0;
+        } else {
+            return i + sumofOutput(i + 1);
+        }
+    }
+}
+
 public class SumofNaturalNumber {
 
 }
