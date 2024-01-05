f0d5aaa5248036982dcf4761063d62e64ad09f07
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-05 17:11:30 +0530

    Java Program

--- recursion/CountDigit.java
+++ recursion/CountDigit.java
@@ -3,15 +3,12 @@ package recursion;
 import java.util.Scanner;
 
 class count {
-    int count(int num) {
-        if (num < 10) {
-            return num;
-        } else {
-            int c = 0;
-            int d = num % 10;
-            c++;
-            count(num / 10);
+    int count(int num, int c) {
+        if (num == 0) {
             return c;
+        } else {
+            int d = num % 10;
+            return count(num / 10, c + 1);
 
         }
     }
@@ -20,7 +17,7 @@ class count {
 public class CountDigit {
     public static void main(String[] args) {
         count Ram = new count();
-        int result = Ram.count(12345);
+        int result = Ram.count(12345, 0);
         System.out.println("Answer:  " + result);
     }
 }
