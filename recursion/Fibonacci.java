7085fdfc39ae44fc75ddb4b04821f1840eb57028
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-05 16:51:10 +0530

    Java Program

--- recursion/Fibonacci.java
+++ recursion/Fibonacci.java
@@ -2,7 +2,7 @@ package recursion;
 import java.util.Scanner;
 class fibo{
     void Disp(int a,int b,int n){
-        if(n==11){
+        if(n>=8){
             System.out.println("");
             return;
         }
@@ -16,8 +16,8 @@ class fibo{
 public class Fibonacci {
     public static void main(String[]args){
         fibo ob=new fibo();
-        System.out.print("0");
-        System.out.print("1");
-        ob.Disp(0, 1, 1);
+        System.out.print("0 ");
+        System.out.print("1 ");
+        ob.Disp(0, 1, 2);
     }
 }
