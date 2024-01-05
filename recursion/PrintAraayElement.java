17662aecd62be70ee00958b3dc60480c3d51d4ad
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-05 16:56:46 +0530

    Java Program

---
+++ recursion/PrintAraayElement.java
@@ -0,0 +1,26 @@
+package recursion;
+import java.lang.reflect.Array;
+import java.util.Scanner;
+class ArrayElement{
+    void dispArray(int i,int arr[]){
+        if(i>10){
+            return;
+        }
+        else{
+            System.out.println("arr["+i+"] = "+arr[i]);
+            dispArray(i+1, arr);
+        }
+    }
+}
+public class PrintAraayElement {
+    public static void main(String[] args) {
+        Scanner Ayodhya=new Scanner(System.in);
+        int arr[]=new int[10];
+        System.out.println("Enter the Elment in Array");
+        for(int i=0;i<10;i++){
+            arr[i]=Ayodhya.nextInt();
+        }
+        ArrayElement Ram=new ArrayElement();
+        Ram.dispArray(0, arr);
+    }
+}
