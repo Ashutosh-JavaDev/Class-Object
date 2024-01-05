7406159eb42500155a75fa795e64a6282fac1cad
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-05 16:42:02 +0530

    Java Program

---
+++ recursion/Fibonacci.java
@@ -0,0 +1,23 @@
+package recursion;
+import java.util.Scanner;
+class fibo{
+    void Disp(int a,int b,int n){
+        if(n==11){
+            System.out.println("");
+            return;
+        }
+        else{
+            int c=a+b;
+            System.out.print(c+" ");
+            Disp(b, c, n+1);
+        }
+    }
+}
+public class Fibonacci {
+    public static void main(String[]args){
+        fibo ob=new fibo();
+        System.out.print("0");
+        System.out.print("1");
+        ob.Disp(0, 1, 1);
+    }
+}
