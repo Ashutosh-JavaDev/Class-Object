39dfc4eca37ab7031611906443e66176c8e62111
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-06 21:52:21 +0530

    Java Program

--- Sorting/MergeSort.java
+++ Sorting/MergeSort.java
@@ -1,12 +1,12 @@
 package Sorting;
 
 class merge {
-    void Merge(int arr[], int s, int e) {
-        int mid = s + (e - s) / 2;
-        int i = s, j = mid, k = 0;
-        int arr2[] = new int[e + 1];
+    void Merge(int arr[], int s,int mid, int e) {
+         mid = s + (e - s) / 2;
+        int i = s, j = mid+1, k = 0;
+        int arr2[] = new int[e -s+ 1];
         while (i <= mid && j <= e) {
-            if (arr[i] < arr[j]) {
+            if (arr[i] >     arr[j]) {
                 arr2[k] = arr[i];
                 i++;
                 k++;
@@ -16,21 +16,28 @@ class merge {
                 k++;
             }
         }
-        while (i < mid) {
+        while (i <= mid) {
             arr2[k] = arr[i];
             i++;
             k++;
         }
-        while (j < e) {
+        while (j <= e) {
             arr2[k] = arr[j];
             j++;
             k++;
         }
-        System.out.println("Array after Sort");
         for (k = 0; k < arr2.length; k++) {
             arr[s + k] = arr2[k];
         }
-        System.out.println();
+    }
+
+    void mergeSort(int arr[], int s, int e) {
+        if (s < e) {
+            int mid = s + (e - s) / 2;
+            mergeSort(arr, s, mid);
+            mergeSort(arr, mid + 1, e);
+            Merge(arr, s, mid, e);
+        }
     }
 
 }
@@ -39,7 +46,7 @@ public class MergeSort {
     public static void main(String[] args) {
         merge ob = new merge();
         int arr[] = { 7, 8, 1, 2, 5, 9 };
-        ob.Merge(arr, 0, arr.length - 1);
+        ob.mergeSort(arr, 0, arr.length - 1);
         System.out.println("Array after Sort");
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
