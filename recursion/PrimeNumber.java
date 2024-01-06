0d236f7aff670af813cc06fc3db607e718294ddd
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-06 10:13:13 +0530

    Java Program

--- recursion/PrimeNumber.java
+++ recursion/PrimeNumber.java
@@ -1,22 +1,25 @@
 package recursion;
-class primeNumber{
-    void PrimeNumbercheck(int num,int i,int count){
-        if(i>num){
-            return;
-        }
-        else{
-            if(num%i==0){
-                PrimeNumbercheck(num, i+1, count+1);
-                if(count==2){
-                    System.out.println("Number is  Prime");
-                }
-                else{
-                    System.out.println("Number is not Prime");
-                }
+
+class primeNumber {
+    void PrimeNumbercheck(int num, int i, int count) {
+        if (i > num) {
+            if (count == 2) {
+                System.out.println("Number is Prime");
+            } else {
+                System.out.println("Number is not Prime");
             }
+            return;
+        } else if (num % i == 0) {
+            PrimeNumbercheck(num, i + 1, count + 1);
+        } else {
+            PrimeNumbercheck(num, i + 1, count);
         }
     }
 }
+
 public class PrimeNumber {
-    
+    public static void main(String[] args) {
+        primeNumber ob = new primeNumber();
+        ob.PrimeNumbercheck(15, 1, 0);
+    }
 }
