b5bb526fa16187dff7d951083f8f12a116445377
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-06 12:14:51 +0530

    Java Program

--- recursion/BinarySearch.java
+++ recursion/BinarySearch.java
@@ -19,5 +19,10 @@ class search{
     }
 }
 public class BinarySearch {
-    
+    public static void main(String[]args){
+        int arr[]={1,2,3,4,5,6,7,8,9,20};
+        search ob=new search();
+        int result=ob.khojo(arr, 2, 0, arr.length-1);
+        System.out.println("Answer: "+result);
+    }
 }
