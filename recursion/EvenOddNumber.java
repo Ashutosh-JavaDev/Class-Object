a686306616c376e9a5918499c3ba637f36b87d56
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-06 11:10:01 +0530

    Java Program

--- recursion/EvenOddNumber.java
+++ recursion/EvenOddNumber.java
@@ -2,14 +2,17 @@ package recursion;
 
 class check {
     void evenodd(int arr[], int size) {
-        if (size-1 < 0) {
+        if (size - 1 < 0) {
             System.out.println("");
             return;
         } else {
             if (arr[size - 1] % 2 == 0) {
+                System.out.println("__________________________________________________");
                 System.out.print(arr[size - 1] + " ");
                 evenodd(arr, size - 1);
             } else {
+                System.out.println("--------------------------------------------------");
+
                 System.out.print(arr[size - 1] + " ");
 
                 evenodd(arr, size - 2);
@@ -19,9 +22,9 @@ class check {
 }
 
 public class EvenOddNumber {
-    public static void main(String[]args){
-        check ob=new check();
-        int arr[]={1,2,3,4,5,6,7,8,9,10};
+    public static void main(String[] args) {
+        check ob = new check();
+        int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
         ob.evenodd(arr, 10);
     }
 }
