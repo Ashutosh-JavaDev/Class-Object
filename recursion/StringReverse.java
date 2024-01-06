431d121fab8e098a808a2515f8b5eb5df3ddc916
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-06 09:33:21 +0530

    Java Program

--- recursion/StringReverse.java
+++ recursion/StringReverse.java
@@ -1,21 +1,22 @@
 package recursion;
 
 import java.util.Scanner;
-class reverse{
-    void revString(String str,int l){
-        if(l==0){
+
+class reverse {
+    void revString(String str, int l) {
+        if (l == 0) {
             return;
-        }
-        else{
-            l=str.length();
-            System.out.println(str.charAt(l-1));
-            revString(str,l-1);
+        } else {
+            System.out.print(str.charAt(l - 1));
+            revString(str, l - 1);
         }
     }
 }
+
 public class StringReverse {
     public static void main(String[] args) {
-        reverse ob=new reverse();
+        reverse ob = new reverse();
         ob.revString("Ashutosh", "Ashutosh".length());
+        System.out.println();
     }
 }
