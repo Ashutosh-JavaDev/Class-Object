a85bc2db11ae742f94eb545e843b8447fcbc18ae
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-06 09:18:58 +0530

    Java Program

--- recursion/StringReverse.java
+++ recursion/StringReverse.java
@@ -2,7 +2,15 @@ package recursion;
 
 import java.util.Scanner;
 class reverse{
-    
+    void revString(String str){
+        if(str.length()<2){
+            return;
+        }
+        else{
+            System.out.println(str.charAt(str.length()-1));
+            revString(str);
+        }
+    }
 }
 public class StringReverse {
 
