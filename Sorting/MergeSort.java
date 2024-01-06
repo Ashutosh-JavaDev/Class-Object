8875df8c2bdd7f2aa66149248e3d2dd8e0f0326d
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-06 21:12:56 +0530

    Java Program

--- Sorting/MergeSort.java
+++ Sorting/MergeSort.java
@@ -4,8 +4,8 @@ class merge {
     void Merge(int arr[], int s, int e) {
         int mid = s + (e - s) / 2;
         int i = s, j = mid, k = 0;
-        int arr2[] = new int[e-s+1];
-        while (i < mid && j < e) {
+        int arr2[] = new int[e + 1];
+        while (i <= mid && j <= e) {
             if (arr[i] < arr[j]) {
                 arr2[k] = arr[i];
                 i++;
@@ -28,17 +28,22 @@ class merge {
         }
         System.out.println("Array after Sort");
         for (k = 0; k < arr2.length; k++) {
-            System.out.print(arr2[k] + " ");
+            arr[s + k] = arr2[k];
         }
+        System.out.println();
     }
 
 }
 
 public class MergeSort {
-public static void main(String[]args){
-    merge ob=new merge();
-    int arr[]={7,8,1,2,5,9};
-    ob.Merge(arr, 0, arr.length-1);
+    public static void main(String[] args) {
+        merge ob = new merge();
+        int arr[] = { 7, 8, 1, 2, 5, 9 };
+        ob.Merge(arr, 0, arr.length - 1);
+        System.out.println("Array after Sort");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
 
-}
+    }
 }
