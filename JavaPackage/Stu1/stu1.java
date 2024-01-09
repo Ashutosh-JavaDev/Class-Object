c8c6c1835536a7e0bce25d4ffe7a3c2336890f72
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-09 11:10:09 +0530

    Java Program

--- JavaPackage/Stu1/stu1.java
+++ JavaPackage/Stu1/stu1.java
@@ -3,21 +3,14 @@ package JavaPackage.Stu1;
 import java.util.Scanner;
 
 public class stu1 {
-    public void get1() {
+    public int[] get1() {
         Scanner sc = new Scanner(System.in);
         int arr[] = new int[5];
         System.out.println("Enter the data in the Array");
         for (int i = 0; i < 5; i++) {
             arr[i] = sc.nextInt();
         }
-    }
-
-    public int put1() {
-        Scanner sc = new Scanner(System.in);
-        int arr[] = new int[5];
-        System.out.println("Enter the data in the Array");
-        for(int i=0;i<5;i++){
-            return arr[i];
-        }
+        System.out.println("Data Display");
+        return arr;
     }
 }
