5c0a9ad949c2dd518492a7a91a9a1b26ee036a82
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-09 10:59:49 +0530

    java Program

--- JavaPackage/abc1.java
+++ JavaPackage/abc1.java
@@ -1,5 +1,6 @@
 package JavaPackage;
 import JavaPackage.Stu1.stu1;
+import JavaPackage.Stu2.stu2;
 public class abc1 {
    public static void main(String[] args) {
     System.out.println("This Message is from Main Function");
