f531968146118ef924391aa81ae323cccc91dbf2
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-09 11:01:19 +0530

    Changes in Website

--- JavaPackage/abc1.java
+++ JavaPackage/abc1.java
@@ -3,6 +3,10 @@ import JavaPackage.Stu1.stu1;
 import JavaPackage.Stu2.stu2;
 public class abc1 {
    public static void main(String[] args) {
+    stu1 ob1=new stu1();
+    stu2 ob2=new stu2();
     System.out.println("This Message is from Main Function");
+    ob1.get1();
+    ob2.get2();
    } 
 }
