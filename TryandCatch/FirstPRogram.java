b864871170534e9f8cdc74467dd32d60328482a7
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-11 09:58:40 +0530

    Java Program

---
+++ TryandCatch/FirstPRogram.java
@@ -0,0 +1,32 @@
+package TryandCatch;
+
+import java.util.Scanner;
+
+class aa1 {
+    int arr[] = new int[4];
+
+    void disp() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Array");
+        for (int i = 0; i < 4; i++) {
+            try {
+                arr[3] = 12345;
+                arr[i] = sc.nextInt();
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println(e);
+            } finally {
+                System.out.println("Array Index");
+                for (int j = 0; j < 4; j++) {
+                    System.out.println("arr[" + j + "] = " + arr[j]);
+                }
+            }
+        }
+    }
+}
+
+public class FirstPRogram {
+    public static void main(String[] args) {
+        aa1 ob = new aa1();
+        ob.disp();
+    }
+}
