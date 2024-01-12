4584bff3cbe5696e5cacc1e05f96812b98bd776c
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-12 10:21:29 +0530

    Java Program

--- TryandCatch/SumofArray.java
+++ TryandCatch/SumofArray.java
@@ -6,26 +6,24 @@ class aa2 {
     void sumofArray() {
         Scanner sc = new Scanner(System.in);
         int arr[] = new int[5];
-        int sum=0;
+        int sum = 0;
         System.out.println("Enter the Array in the Array");
-        for(int i=0;i<5;i++){
-          try{
-            arr[i]=sc.nextInt();
-            for(int j=0;j<5;j++){
-                sum+=arr[i];
+        for (int i = 0; i < 6; i++) {
+            try {
+                arr[i] = sc.nextInt();
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println(e);
             }
-          }
-          catch(ArrayIndexOutOfBoundsException e){
-            System.out.println(e);
-          }
         }
-        
-       
+        for (int j = 0; j < 5; j++) {
+            sum += arr[j];
+        }
+        System.out.println("Sum:    " + sum);
     }
 }
 
 public class SumofArray {
-    public static void main(String[]args){
+    public static void main(String[] args) {
         new aa2().sumofArray();
     }
 }
