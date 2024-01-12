dbf5c708367aa660fb71718512deb2478fd81aa4
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-12 12:12:46 +0530

    Java Program

---
+++ exception/ageException.java
@@ -0,0 +1,11 @@
+package exception;
+import java.util.Scanner;
+class detail{
+    public void get(){
+        Scanner sc=new Scanner(System.in);
+        
+    }
+}
+public class ageException {
+    
+}
