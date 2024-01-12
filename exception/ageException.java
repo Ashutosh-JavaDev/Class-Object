aa82603a32b5a0b04b8b07ae6015a42207d35d08
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-12 12:13:56 +0530

    Java Program

--- exception/ageException.java
+++ exception/ageException.java
@@ -3,7 +3,10 @@ import java.util.Scanner;
 class detail{
     public void get(){
         Scanner sc=new Scanner(System.in);
-        
+        System.out.println("Enter Name");
+        String Name=sc.nextLine();
+        System.out.println("Enter Age");
+        int age=sc.nextInt();
     }
 }
 public class ageException {
