cd6df2523c4f30dcc172c846a697d294f403d4e3
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-12 10:40:52 +0530

    Java Program

---
+++ exception/Arthemeticexcep.java
@@ -0,0 +1,40 @@
+package exception;
+
+import java.util.Scanner;
+
+class excep {
+    int a, b, c;
+
+    void takea(int a) {
+        this.a = a;
+    }
+
+    void takeb(int b) {
+        this.b = b;
+    }
+
+    int puta() {
+        return a;
+    }
+
+    int putb() {
+        return b;
+    }
+
+    void disp() {
+        try {
+            c = a / b;
+            System.out.println("Result:     " + c);
+        } catch (ArithmeticException e) {
+            System.out.println(e);
+        }
+    }
+}
+
+public class Arthemeticexcep {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value in a");
+        int a = sc.nextInt();
+    }
+}
