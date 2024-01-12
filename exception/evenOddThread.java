cc696242c93c47706e1a0a8412effc75d36eb73e
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-12 11:12:07 +0530

    Java Program

---
+++ exception/evenOddThread.java
@@ -0,0 +1,17 @@
+package exception;
+import java.util.Scanner;
+class even{
+    void run(){
+        Scanner sc=new Scanner(System.in);
+        int arr[]=new int[10];
+        
+        for(int i=0;i<10;i++){
+            if(arr[i]%2==0){
+
+            }
+        }
+    }
+}
+public class evenOddThread {
+    
+}
