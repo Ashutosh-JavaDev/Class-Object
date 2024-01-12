3c18c814172baf9e18ace14231eba0e3c6cc4fb4
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-12 11:18:44 +0530

    Java Program

--- exception/evenOddThread.java
+++ exception/evenOddThread.java
@@ -6,9 +6,17 @@ class even{
         int arr[]=new int[10];
 
         try{
+            System.out.println("Enter the value in the Array");
             for(int i=0;i<10;i++){
-                
+                arr[i]=sc.nextInt();
             }
+            System.out.println("Even Number");
+            for(int i=0;i<10;i++){
+                if(arr[i]%2==0){
+                    System.out.print(arr[i]+" ");
+                }
+            }
+
         }
         catch(ArrayIndexOutOfBoundsException e){
 
