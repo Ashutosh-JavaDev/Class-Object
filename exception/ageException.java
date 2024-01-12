044a22b81d34860d485cf1d54fd91a3e396be3ea
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-12 12:33:38 +0530

    Java Program

--- exception/ageException.java
+++ exception/ageException.java
@@ -1,25 +1,32 @@
 package exception;
+
+import java.util.InputMismatchException;
 import java.util.Scanner;
-class detail{
-    public void get(){
-        Scanner sc=new Scanner(System.in);
+
+class detail {
+    public void get() {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter Name");
-        String Name=sc.nextLine();
+        String Name = sc.nextLine();
         System.out.println("Enter Age");
-        int age=sc.nextInt();
-      try{
-       if(age>0){
-        System.out.println("Age:    "+age);
-       }
-      }
-      catch(IllegalArgumentException e){
-        System.out.println(e);
-      }
+        int age = sc.nextInt();
+
+        try {
+            if (age > 0) {
+                System.out.println("Age: " + age);
+            }
+            else{
+                System.out.println("Invalid Age");
+            }
+        } catch (InputMismatchException e) {
+            System.out.println(e);
+        }
     }
 }
+
 public class ageException {
-    public static void main(String[]args){
-        detail ob=new detail();
+    public static void main(String[] args) {
+        detail ob = new detail();
         ob.get();
     }
 }
