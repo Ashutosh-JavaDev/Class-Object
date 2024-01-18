306d21e5c5570882929910d5d7cbbfc9221cb805
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-18 11:44:36 +0530

    Java Program

---
+++ JavaPackage/DisplayData.java
@@ -0,0 +1,9 @@
+package JavaPackage;
+import JavaPackage.Stu1.creatingData;
+import JavaPackage.Stu2.PuttingData;
+public class DisplayData {
+    public static void main(String[] args) {
+        creatingData ob3=new creatingData();
+        PuttingData ob4=new PuttingData();
+    }
+}
