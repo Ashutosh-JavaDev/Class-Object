da2811ff0b8974ffa66880c388c67981405d297b
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-18 11:37:48 +0530

    Java Program

--- JavaPackage/Stu1/creatingData.java
+++ JavaPackage/Stu1/creatingData.java
@@ -15,8 +15,11 @@ class createData{
 }
 public class creatingData {
     public static void main(String[] args) {
-        createData ob=new createData();
-        System.out.println("How Many Data want to add in Object");
-        
+        createData ob[]=new createData[5];
+        System.out.println("Enter 5 Student Detail");
+        for(int i=0;i<5;i++){
+            ob[i].takeInput();
+        }
+
     }
 }
