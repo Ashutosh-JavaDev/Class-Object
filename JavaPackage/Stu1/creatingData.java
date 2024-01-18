624338d25e1b9004bf6d8e046f0601e66c0e05c9
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-18 11:27:19 +0530

    Java Program

---
+++ JavaPackage/Stu1/creatingData.java
@@ -0,0 +1,18 @@
+package JavaPackage.Stu1;
+import java.util.Scanner;
+class createData{
+    String Name;
+    int age,roll;
+    void takeInput(){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter Name");
+        Name=sc.nextLine();
+        System.out.println("Enter Roll");
+        roll=sc.nextInt();
+        System.out.println("Enter Age");
+        age=sc.nextInt();
+    }
+}
+public class creatingData {
+    
+}
