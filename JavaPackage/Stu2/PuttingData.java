9ba90ca70eb05070e17e2fa78881cceaf4426b6d
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-18 11:39:18 +0530

    Java Program

---
+++ JavaPackage/Stu2/PuttingData.java
@@ -0,0 +1,15 @@
+package JavaPackage.Stu2;
+class putData{
+    String Name;
+    int age,roll;
+    void disp(){
+        System.out.println("Name:   "+Name);
+        System.out.println("Age:    "+age);
+        System.out.println("Roll:   "+roll);
+    }
+}
+public class PuttingData {
+    public static void main(String[] args) {
+        
+    }
+}
