1e5a33fe363f4d7a8950cfff0d882369c7c33ce4
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-18 11:59:58 +0530

    Java Program

--- JavaPackage/Stu2/PuttingData.java
+++ JavaPackage/Stu2/PuttingData.java
@@ -1,22 +1,14 @@
 package JavaPackage.Stu2;
-class putData{
-    String Name;
-    int age,roll;
-    void disp(){
-        System.out.println("Name:   "+Name);
-        System.out.println("Age:    "+age);
-        System.out.println("Roll:   "+roll);
-    }
-}
-public class PuttingData {
-    public static void main(String[] args) {
-        putData ram[]=new putData[5];
-        System.out.println("---------------------------------------------");
-        System.out.println("Student details");
 
-        for(int i=0;i<5;i++){
-            ram[i]=new putData();
-            System.out.println(ram[i]);
+class PuttingData {
+    String Name;
+    int age, roll;
+
+    void disp() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Name:   " + Name);
+            System.out.println("Age:    " + age);
+            System.out.println("Roll:   " + roll);
         }
     }
 }
