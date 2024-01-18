e1dd498b813ea77ba45b260d64b237fd5efdd7d2
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-18 12:33:44 +0530

    Java Program

--- JavaPackage/Stu1/displaySentence.java
+++ JavaPackage/Stu1/displaySentence.java
@@ -3,10 +3,14 @@ package JavaPackage.Stu1;
 import java.util.Scanner;
 
 public class displaySentence {
+    String Name;
     public void disp() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter sentence");
         String Name=sc.nextLine();
+    }
+    public String getName(){
+        return Name;
     }
 
 }
