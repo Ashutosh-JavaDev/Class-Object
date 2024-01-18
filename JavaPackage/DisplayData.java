a7c8515313862df27a3567c7f8f63da0c1308ed0
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-18 12:27:33 +0530

    Java Program

--- JavaPackage/DisplayData.java
+++ JavaPackage/DisplayData.java
@@ -1,20 +1,11 @@
 package JavaPackage;
 
 import JavaPackage.Stu1.creatingData;
-import JavaPackage.Stu2.PuttingData;
-
 public class DisplayData {
     public static void main(String[] args) {
         creatingData ob3 = new creatingData();
-        PuttingData ob4 = new PuttingData();
-
-        // Take input
         System.out.println("Enter Student Detail");
         ob3.takeInput();
-
-        // Display data
-        System.out.println("---------------------------------------------");
-        System.out.println("Student details");
-        ob4.disp(ob3);
+        ob3.disp();
     }
 }
--- JavaPackage/Stu1/creatingData.java
+++ JavaPackage/Stu1/creatingData.java
@@ -2,32 +2,43 @@ package JavaPackage.Stu1;
 import java.util.Scanner;
 
 public class creatingData {
-    String Name;
-    int age, roll;
+    String[] names;
+    int[] ages, rolls;
+    int numberOfStudents;
 
     public void takeInput() {
         Scanner sc = new Scanner(System.in);
-        for (int i = 0; i < 5; i++) {
-            System.out.println("Enter Name");
-            Name = sc.nextLine();
-            sc.nextLine();
-            System.out.println("Enter Roll");
-            roll = sc.nextInt();
-            System.out.println("Enter Age");
-            age = sc.nextInt();
+
+        System.out.println("Enter the number of students: ");
+        numberOfStudents = sc.nextInt();
+
+        names = new String[numberOfStudents];
+        ages = new int[numberOfStudents];
+        rolls = new int[numberOfStudents];
+
+        for (int i = 0; i < numberOfStudents; i++) {
+            sc.nextLine(); // Consume the newline character
+
+            System.out.println("Enter Name for student " + (i + 1) + ": ");
+            names[i] = sc.nextLine();
+
+            System.out.println("Enter Roll for student " + (i + 1) + ": ");
+            rolls[i] = sc.nextInt();
+
+            System.out.println("Enter Age for student " + (i + 1) + ": ");
+            ages[i] = sc.nextInt();
         }
     }
 
-    // Getter methods to access the details
-    public String getName() {
-        return Name;
-    }
+    public void disp() {
+        System.out.println("Students Details:   ");
+        System.out.println("------------------------------");
 
-    public int getAge() {
-        return age;
-    }
-
-    public int getRoll() {
-        return roll;
+        for (int i = 0; i < numberOfStudents; i++) {
+            System.out.println("Name:   " + names[i]);
+            System.out.println("Age:    " + ages[i]);
+            System.out.println("Roll:   " + rolls[i]);
+            System.out.println("------------------------------");
+        }
     }
 }
