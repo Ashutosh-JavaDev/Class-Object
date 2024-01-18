ac5cfd00cefd3660bfb888b901489155b7b686d5
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-18 12:06:13 +0530

    Java Program

--- JavaPackage/Stu1/creatingData.java
+++ JavaPackage/Stu1/creatingData.java
@@ -1,13 +1,14 @@
 package JavaPackage.Stu1;
 import java.util.Scanner;
-class createData{
+public class creatingData{
     String Name;
     int age,roll;
-    void takeInput(){
+    public void takeInput(){
         Scanner sc=new Scanner(System.in);
        for(int i=0;i<5;i++){
         System.out.println("Enter Name");
         Name=sc.nextLine();
+        sc.nextLine();
         System.out.println("Enter Roll");
         roll=sc.nextInt();
         System.out.println("Enter Age");
--- JavaPackage/Stu2/PuttingData.java
+++ JavaPackage/Stu2/PuttingData.java
@@ -4,7 +4,7 @@ public class PuttingData {
     String Name;
     int age, roll;
 
-    void disp() {
+  public  void disp() {
         for (int i = 0; i < 5; i++) {
             System.out.println("Name:   " + Name);
             System.out.println("Age:    " + age);
