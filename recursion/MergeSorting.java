b9bfe8ee1d3c0064386d0fcfb17caeabab365a9d
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-24 14:30:58 +0530

    Program

--- recursion/MergeSorting.java
+++ recursion/MergeSorting.java
@@ -1,52 +1,49 @@
 package recursion;
-class mergeSort{
-    void merging(int arr[],int s,int mid,int e){
-        mid=s+(e-s)/2;
-        int mergeArray[]=new int[e-s+1];
-        int i=s,j=mid+1,k=0;
-        while(i<=mid&& j<=e){
-            if(arr[i]>arr[j]){
-                mergeArray[k]=arr[i];
-                i++;
-                k++;
-            }
-            else{
-                mergeArray[k]=arr[j];
-                j++;
-                k++;
+
+class mergeSort {
+    void merging(int arr[], int s, int mid, int e, int[] mergeArray) {
+        mid = s + (e - s) / 2;
+        int i = s, j = mid + 1, k = 0;
+        while (i <= mid && j <= e) {
+            if (arr[i] > arr[j]) {
+                mergeArray[k++] = arr[i++];
+            } else {
+                mergeArray[k++] = arr[j++];
             }
         }
-        while(i<=mid){
-            mergeArray[k]=arr[i];
-            i++;
-            k++;
+        while (i <= mid) {
+            mergeArray[k++] = arr[i++];
+
         }
-        while(j<=e){
-            mergeArray[k]=arr[j];
-            j++;k++;
+        while (j <= e) {
+            mergeArray[k++] = arr[j++];
+            // j++;k++;
         }
-        for(i=0,j=s;i<mergeArray.length;i++,j++){
-            arr[j]=mergeArray[i];
+        for (i = 0, j = s; i < k; i++, j++) {
+            arr[j] = mergeArray[i];
         }
 
     }
-    void merged(int arr[],int s,int e){
-        if(s<e){
-            int mid=s+(e-s)/2;
-            merged(arr, s, mid);
-            merged(arr, mid+1, e);
-            merging(arr, s, mid, e);
+
+    void merged(int arr[], int s, int e, int[] mergeArray) {
+        if (s < e) {
+            int mid = s + (e - s) / 2;
+            merged(arr, s, mid, mergeArray);
+            merged(arr, mid + 1, e, mergeArray);
+            merging(arr, s, mid, e, mergeArray);
         }
     }
 }
+
 public class MergeSorting {
     public static void main(String[] args) {
-        mergeSort ob=new mergeSort();
-        int arr[]={3,1,5,2,64,23,0,89};
-        ob.merged(arr, 0, arr.length-1);
+        mergeSort ob = new mergeSort();
+        int arr[] = { 3, 1, 5, 2, 64, 23, 0, 89 };
+        int mergeArray[] = new int[arr.length];
+        ob.merged(arr, 0, arr.length - 1, mergeArray);
         System.out.println("Sorted Array");
-        for(int i=0;i<arr.length;i++){
-            System.out.print(arr[i]+" ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
         }
         System.out.println();
     }
