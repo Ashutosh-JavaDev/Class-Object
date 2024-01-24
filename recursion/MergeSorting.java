2880211e0818059462f0f9ad04fbcf535032949c
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-24 13:11:01 +0530

    Java Program:

--- recursion/MergeSorting.java
+++ recursion/MergeSorting.java
@@ -3,7 +3,29 @@ class mergeSort{
     void merging(int arr[],int s,int mid,int e){
         mid=s+(e-s)/2;
         int mergeArray[]=new int[e-s+1];
-        
+        int i=s,j=mid+1,k=0;
+        while(i<=mid&& j<=e){
+            if(arr[i]>=arr[j]){
+                mergeArray[k]=arr[i];
+                i++;
+                k++;
+            }
+            else{
+                mergeArray[k]=arr[j];
+                j++;
+                k++;
+            }
+        }
+        while(i<=mid){
+            mergeArray[k]=arr[i];
+            i++;
+            k++;
+        }
+        while(j<=e){
+            mergeArray[k]=arr[j];
+            j++;k++;
+        }
+
     }
 }
 public class MergeSorting {
