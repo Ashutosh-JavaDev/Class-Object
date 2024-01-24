88361bd937fd2df641488a95f0793206629619cf
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-24 13:20:42 +0530

    Java Program:

--- recursion/MergeSorting.java
+++ recursion/MergeSorting.java
@@ -30,11 +30,19 @@ class mergeSort{
         }
 
     }
+    void merged(int arr[],int s,int e){
+        if(s>e){
+            int mid=s+(e-s)/2;
+            merged(arr, s, mid);
+            merged(arr, mid+1, e);
+            merging(arr, s, mid, e);
+        }
+    }
 }
 public class MergeSorting {
     public static void main(String[] args) {
         mergeSort ob=new mergeSort();
         int arr[]={3,1,5,2,64,23,0,89};
-        
+        ob.merged(arr, 0, arr.length-1);
     }
 }
